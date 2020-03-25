b5f68852ec9ee71a475b457e603af55db4dbb67b
AntoninJarolim <jarolim.antonin@seznam.cz>
2020-03-25 20:28:48 +0100

    hej hou + delete messages

--- index.js
+++ index.js
@@ -6,9 +6,28 @@ const token = process.env.TOKEN;
 
 const PREFIX = '!';
 
+bot.on( 'ready' , () => {
+    console.log( 'Robí!');
+})
+
+
 
 bot.on('message', message=>{
     message.channel.setPosition(0);
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'hej':
+            message.channel.send('Hou!')
+            break;
+
+        case 'delete':
+            if(!args[1])
+            return message.channel.send('Neni definovan druhy parametr!')
+            else
+            message.channel.bulkDelete(args[1])
+    }
 })
 
 bot.login(token);
