daa2c2f50d9d68f8de74550e5413946580fc5321
AntoninJarolim <jarolim.antonin@seznam.cz>
2020-03-26 19:46:41 +0100

    added funkce smaz kanal

--- index.js
+++ index.js
@@ -31,10 +31,17 @@ bot.on('message', message => {
                 message.channel.send('Hou!')
             }
             break;
+        case 'info':
+            if (args[1] === '--help') {
+                return message.channel.send('Tak sí píííííča?')
+            } else {
+                message.channel.send('Seznam příkazů: \n      hej\n      smazkanal \n      delete \n      spamuj \n      razeni \nPoužij --help pro více info o každém z nich!')
+            } 
+            break;
 
         case 'delete':
             if (!args[1]) {
-                return message.channel.send('Neni definovan druhy parametr!')
+                return message.channel.send('Neni definovan druhy parametr! Použij --help!')
             }
             if (args[1] === '--help') {
                 return message.channel.send('Smaže určitý počet zpráv podle druhého argumentu. Například !delete 5 smaže 5 zpráv.\nTato funkce funguje maximalně na 100 zpráv. Pokud je druhý argument větší než 100, smaže se 100. \nTato funkce má různé omezení od discordu, takže pokud to nefunguje, tak si je vyčerpal!')
@@ -49,29 +56,24 @@ bot.on('message', message => {
             }
             break;
 
-        case 'info':
-            if (args[1] === '--help') {
-                return message.channel.send('Tak sí píííííča?')
-            } else {
-                message.channel.send('Seznam příkazů: \n      hej \n      delete \n      spamuj \n      razeni \nPoužij --help pro více info o každém z nich!')
-            } break;
+        
 
         case 'spamuj':
             if (args[1] === '--help') {
                 return message.channel.send('Ano, tohle je naprosto otravné. V zasade to proste spamuje:)\nPoužij druhý argument pro počet zpráv které je třeba pospamovat.')
             } else {
                 if (!args[1]) {
-                    return message.channel.send('Neni definovan druhy parametr!')
+                    return message.channel.send('Neni definovan druhy parametr! Použij --help!')
                 }
                 for (let i = 0; i < args[1]; i++) {
-                    message.channel.send(i + 'je menší než: ' + args[1])
+                    message.channel.send(i + ' je menší než: ' + args[1])
                 }
             }
             break;
 
         case 'razeni':
             if (!args[1]) {
-                return message.channel.send('Neni definovan druhy parametr!')
+                return message.channel.send('Neni definovan druhy parametr! Použij --help!')
             }
             if (args[1] === '--help') {
                 return message.channel.send('Tato funkce zapne seřazování zpráv podle nejnovější - když někdo napíše do jakéhokoliv kanálu, tak se objeví úplně nahoře.\nPoužij argumenty on/off pro zapnutí a vypnutí. ')
@@ -86,6 +88,21 @@ bot.on('message', message => {
                     message.channel.send('Vypl si řazení podle nejnovejší zprávy!')
                 }
             }
+            break;
+        case 'smazkanal':
+            if(!args[1]){
+                return message.channel.send('Neni definovan druhy parametr! Použij --help!')
+            }
+            if (args[1] === '--help') {
+                return message.channel.send('Tato funkce smaže kanál. \nJako druhý argument definuj přesné jméno kanálu, které chceš smazat.')
+            }
+
+            let nalezenychannel = message.guild.channels.cache.find(channel => channel.name === args[1]);           
+            if (!nalezenychannel){
+                return message.channel.send('Channel s tímto jménem nebyl nalezen!'); 
+            }
+            nalezenychannel.delete();
+            return message.channel.send('Prave si smazal channel ' + args[1] + ' !');   
             break;
     }
 })
